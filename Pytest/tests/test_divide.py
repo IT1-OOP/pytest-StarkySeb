afb96021fec79526f440a61bd6a166cf64049dd0
StarkySeb <sebastian.stary@student.vitkovickastredni.cz>
2025-03-27 08:29:35 +0100

    divide_test

---
+++ Pytest/tests/test_divide.py
@@ -0,0 +1,12 @@
+from src import calculator
+import pytest
+
+
+def test_devide():
+    with pytest.raises(ValueError) as exc:
+        calculator.divide(5, 0)
+    assert str(exc.value) == "Cannot divide by zero!"
+
+def test_divide2():
+    with pytest.raises(ValueError):
+        assert calculator.divide(8,0) == 4
